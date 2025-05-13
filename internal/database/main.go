ccb08593d87511237701dbe43ec2fe5ee42e0fad
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2025-05-13 14:01:04 +0700

    fix: revert db

--- internal/database/main.go
+++ internal/database/main.go
@@ -1,41 +1,41 @@
 package database
 
-import (
-	"fmt"
-	"log"
-	"os"
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
 
-	"github.com/joho/godotenv"
+// 	"github.com/joho/godotenv"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-)
+// 	"gorm.io/driver/postgres"
+// 	"gorm.io/gorm"
+// 	"gorm.io/gorm/logger"
+// )
 
-var DB *gorm.DB
+// var DB *gorm.DB
 
 func InitDB() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USERNAME"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	// dsn := fmt.Sprintf(
+	// 	"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+	// 	os.Getenv("DB_HOST"),
+	// 	os.Getenv("DB_USERNAME"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_NAME"),
+	// 	os.Getenv("DB_PORT"),
+	// )
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info), // Optional: set logging mode
-	})
+	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+	// 	Logger: logger.Default.LogMode(logger.Info), // Optional: set logging mode
+	// })
 
-	if err != nil {
-		log.Fatalf("Error connecting to the database: %v", err)
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error connecting to the database: %v", err)
+	// }
 
-	log.Print("DB connection established successfully.")
+	// log.Print("DB connection established successfully.")
 }
